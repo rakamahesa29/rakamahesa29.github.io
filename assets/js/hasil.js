3a0acfa614081730323c954c3a10155d4dc92a35
Frontend - Macbook Pro 2019 <frontend-macbookpro2019@Frontends-MacBook-Pro.local>
2023-05-14 11:24:53 +0700

    update latest project

--- assets/js/hasil.js
+++ assets/js/hasil.js
@@ -1,27 +1,60 @@
 window.onload = function() {
-    const hasilDiagnosa = JSON.parse(localStorage.getItem("hasilDiagnosa"));
-    const elemHasilDiagnosa = document.getElementById("hasilDiagnosa");
+  // Membaca query string dari URL
+  const urlParams = new URLSearchParams(window.location.search);
+  const diagnosaId = urlParams.get('id');
+
+  const formData = JSON.parse(localStorage.getItem(`formData-${diagnosaId}`));
+  // Mendapatkan data namaLengkap dari formData
+  const namaLengkap = formData ? formData.namaLengkap : null;
   
-    if (hasilDiagnosa) {
-      hasilDiagnosa.sort((a, b) => b.keyakinan - a.keyakinan);
+  // Anda bisa menggunakan variabel namaLengkap sekarang
+  console.log(namaLengkap);
+
+  const namaPasien = document.getElementById("namaPasien");
+  namaPasien.innerHTML = namaLengkap;
   
-      let teksHasil = "Kemungkinan penyakit yang diderita:<br>";
-      const penyakitUtama = hasilDiagnosa.slice(0, 1);
-      for (const diagnosa of penyakitUtama) {
-        teksHasil += `${diagnosa.penyakit} (Keyakinan: ${(diagnosa.keyakinan * 100).toFixed(2)}%)<br>`;
-      }
-  
-      const penyakitLain = hasilDiagnosa.slice(1);
-      if (penyakitLain.length > 0) {
-        teksHasil += "<br>namun ada kemungkinan penyakit lain yang diderita seperti:<br>";
-        for (const diagnosa of penyakitLain) {
-          teksHasil += `${diagnosa.penyakit} (Keyakinan: ${(diagnosa.keyakinan * 100).toFixed(2)}%)<br>`;
-        }
-      }
-  
-      elemHasilDiagnosa.innerHTML = teksHasil;
-    } else {
-      elemHasilDiagnosa.innerHTML = "Tidak dapat mendiagnosa penyakit. Harap konsultasikan dengan dokter.";
+  const hasilDiagnosa = JSON.parse(localStorage.getItem(`hasilDiagnosa-${diagnosaId}`));
+  const elemHasilDiagnosa = document.getElementById("hasilDiagnosa");
+  const elemHasilPenyakitLain = document.getElementById("hasilPenyakitLain");
+
+  if (hasilDiagnosa) {
+    hasilDiagnosa.sort((a, b) => b.keyakinan - a.keyakinan);
+
+    let teksHasil = "kemungkinan penyakit yang kamu miliki adalah ";
+    let namaPenyakitHasil = "Gejala "
+    let deskripsiHasil = "";
+    let teksPenyakitLain = "";
+    const penyakitUtama = hasilDiagnosa.slice(0, 1);
+    for (const diagnosa of penyakitUtama) {
+      teksHasil += `${diagnosa.penyakit} (dengan nilai keyakinan sebesar ${(diagnosa.keyakinan * 100).toFixed(2)}%)<br>`;
+      namaPenyakitHasil += `${diagnosa.penyakit}`;
+      deskripsiHasil += `${diagnosa.deskripsi}`;
     }
+
+    const penyakitLain = hasilDiagnosa.slice(1);
+    if (penyakitLain.length > 0) {
+      teksPenyakitLain += "namun ada kemungkinan penyakit lain yang diderita seperti";
+      for (const diagnosa of penyakitLain) {
+        // teksHasil += `${diagnosa.penyakit} (dengan nilai keyakinan ${(diagnosa.keyakinan * 100).toFixed(2)}%)<br>`;
+        teksPenyakitLain += `${diagnosa.penyakit} (dengan nilai keyakinan <span class="keyakinan-penyakit">${(diagnosa.keyakinan * 100).toFixed(2)}</span>%) `;
+      }
+    }
+
+    elemHasilDiagnosa.innerHTML = teksHasil;
+    document.getElementById("judulHasilPenyakit").innerHTML = namaPenyakitHasil;
+    document.getElementById("hasilPenyakit").innerHTML = deskripsiHasil;
+    document.getElementById("hasilLainDiagnosa").innerHTML = teksPenyakitLain; // Menampilkan penyakit lain
+  } else {
+    elemHasilDiagnosa.innerHTML = "Tidak dapat mendiagnosa penyakit. Harap konsultasikan dengan dokter.";
+    document.getElementById("hasilPenyakit").innerHTML = "";
+    elemHasilPenyakitLain.innerHTML = ""; // Mengosongkan penyakit lain jika tidak ada hasil diagnosa
   }
-  
+}
+
+function backToHome() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const diagnosaId = urlParams.get('id');
+  localStorage.removeItem("gejala");
+  localStorage.removeItem(`formData-${diagnosaId}`);
+  window.open('/index.html');
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,69 +1,144 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const navIconMobile = document.querySelector("#nav-icon-mobile");
+document.addEventListener("DOMContentLoaded", async function () {
 
+  const navIconMobile = document.querySelector("#nav-icon-mobile");
   navIconMobile.addEventListener("click", function () {
     this.classList.toggle("open");
   });
-});
 
-fetch("./data/gejala_penyakit.json")
-  .then((response) => response.json())
-  .then((data) => {
-    const contentElement = document.querySelector(".gejala-penyakit");
-    let uniqueGejala = new Map();
+  async function ambilDataGejalaPenyakit() {
+    const response = await fetch("./data/gejala_penyakit.json");
+    const data = await response.json();
+    return data;
+  }
 
-    // Melakukan loop melalui setiap penyakit
-    data.penyakit.forEach((penyakit) => {
-      // Melakukan loop melalui setiap gejala
-      penyakit.gejala.forEach((gejala) => {
-        // Menambahkan gejala ke map unik jika belum ada
-        if (!uniqueGejala.has(gejala.nama)) {
-          uniqueGejala.set(gejala.nama, gejala);
+  function min(a, b) {
+    return Math.min(a, b);
+  }
+
+  function fuzzyDiagnosa(gejalaPasien, gejalaPenyakit) {
+    const hasilDiagnosa = [];
+  
+    for (const penyakit of gejalaPenyakit.penyakit) {
+      let minKeyakinan = 1;
+  
+      for (const gejalaObj of penyakit.gejala) {
+        if (gejalaPasien.find(gejala => gejala.nama === gejalaObj.nama)) {
+          minKeyakinan = min(minKeyakinan, gejalaObj.keyakinan);
         }
-      });
+      }
+  
+      if (minKeyakinan !== 1) {
+        hasilDiagnosa.push({
+          penyakit: penyakit.nama,
+          keyakinan: minKeyakinan,
+          deskripsi: penyakit.deskripsi
+        });
+      }
+    }
+  
+    hasilDiagnosa.sort((a, b) => b.keyakinan - a.keyakinan);
+    console.log(hasilDiagnosa);
+    return hasilDiagnosa;
+  }
+
+  async function main() {
+
+    const namaLengkap = document.querySelector("#inputName").value;
+    const nomerTelepon = document.querySelector("#inputPhoneNumber").value;
+    const email = document.querySelector("#inputEmail").value;
+    const alamatLengkap = document.querySelector("#inputFullAddress").value;
+  
+    // Simpan data form di dalam sebuah objek
+    const formData = {
+      namaLengkap,
+      nomerTelepon,
+      email,
+      alamatLengkap
+    };
+    
+   // Membuat ID unik untuk setiap diagnosa
+   const diagnosaId = new Date().getTime();
+
+   // Menyimpan data form dan gejala dengan ID unik
+   localStorage.setItem(`formData-${diagnosaId}`, JSON.stringify(formData));
+ 
+   const gejalaPenyakit = await ambilDataGejalaPenyakit();
+   const gejalaPasien = JSON.parse(localStorage.getItem("gejala")) || [];
+   const hasilDiagnosa = fuzzyDiagnosa(gejalaPasien, gejalaPenyakit);
+   localStorage.setItem(`hasilDiagnosa-${diagnosaId}`, JSON.stringify(hasilDiagnosa));
+   window.open(`hasil-diagnosa-penyakit.html?id=${diagnosaId}`, "_blank");
+  }
+
+  const data = await ambilDataGejalaPenyakit();
+
+  const contentElement = document.querySelector(".gejala-penyakit");
+  let uniqueGejala = new Map();
+
+  // Melakukan loop melalui setiap penyakit
+  data.penyakit.forEach((penyakit) => {
+    // Melakukan loop melalui setiap gejala
+    penyakit.gejala.forEach((gejala) => {
+      // Menambahkan gejala ke map unik jika belum ada
+      if (!uniqueGejala.has(gejala.nama)) {
+        uniqueGejala.set(gejala.nama, gejala);
+      }
     });
+  });
 
-    let idCounter = 0; // Counter untuk membuat id dinamis
+  let idCounter = 0; // Counter untuk membuat id dinamis
 
-    // Loop melalui setiap gejala unik dan menambahkan button ke HTML
-    uniqueGejala.forEach((gejala, gejalaName) => {
-      const gejalaElement = document.createElement("button");
-      gejalaElement.textContent = gejalaName;
+  // Loop melalui setiap gejala unik dan menambahkan button ke HTML
+  uniqueGejala.forEach((gejala, gejalaName) => {
+    const gejalaElement = document.createElement("button");
+    gejalaElement.textContent = gejalaName;
 
-      // Menambahkan atribut data ke button
-      gejalaElement.setAttribute("data-value-name", gejala.nama);
-      gejalaElement.setAttribute("data-value-keyakinan", gejala.keyakinan);
+    // Menambahkan atribut data ke button
+    gejalaElement.setAttribute("data-value-name", gejala.nama);
+    gejalaElement.setAttribute("data-value-keyakinan", gejala.keyakinan);
 
-      // Menambahkan id dinamis ke button
-      gejalaElement.id = `gejalaButton${idCounter++}`;
+    // Menambahkan id dinamis ke button
+    gejalaElement.id = `gejalaButton${idCounter++}`;
 
-      gejalaElement.onclick = function() {
-        // Cek apakah button sudah dipilih
-        if (this.classList.contains("button-selected")) {
-          // Jika sudah dipilih, hapus class dan hapus data dari localStorage
-          this.classList.remove("button-selected");
-          localStorage.removeItem("namaGejala");
-          localStorage.removeItem("nilaiKeyakinan");
-          console.log('Data dihapus dari localStorage');
-        } else {
-          // Jika belum dipilih, tambahkan class dan simpan data ke localStorage
-          this.classList.add("button-selected");
-      
-          const gejalaData = {
-            nama: this.getAttribute("data-value-name"),
-            keyakinan: parseFloat(this.getAttribute("data-value-keyakinan"))
-          };
-          localStorage.setItem("namaGejala", JSON.stringify(gejalaData.nama));
-          localStorage.setItem("nilaiKeyakinan", JSON.stringify(gejalaData.keyakinan));
+    gejalaElement.onclick = function() {
+      // Cek apakah button sudah dipilih
+      if (this.classList.contains("button-selected")) {
+        // Jika sudah dipilih, hapus class dan hapus data dari localStorage
+        this.classList.remove("button-selected");
+          
+        let storedGejala = JSON.parse(localStorage.getItem("gejala")) || [];
+        storedGejala = storedGejala.filter(gejala => gejala.nama !== this.getAttribute("data-value-name"));
         
-          let storedNamaGejala = localStorage.getItem("namaGejala");
-          let storedNilaiKeyakinan = localStorage.getItem("nilaiKeyakinan");
-          console.log(`Data disimpan ke localStorage: ${storedNamaGejala},${storedNilaiKeyakinan}`);
-        }
-      };
-      
+        localStorage.setItem("gejala", JSON.stringify(storedGejala));
+        console.log('Data dihapus dari localStorage');
+      } else {
+        // Jika belum dipilih, tambahkan class dan simpan data ke localStorage
+        this.classList.add("button-selected");
 
-      contentElement.appendChild(gejalaElement);
-    });
-  })
-  .catch((error) => console.error("Error:", error));
+        const gejalaData = {
+          nama: this.getAttribute("data-value-name"),
+          keyakinan: parseFloat(this.getAttribute("data-value-keyakinan"))
+        };
+        
+        let storedGejala = JSON.parse(localStorage.getItem("gejala")) || [];
+        storedGejala.push(gejalaData);
+        
+        localStorage.setItem("gejala", JSON.stringify(storedGejala));
+        console.log(`Data disimpan ke localStorage: ${JSON.stringify(storedGejala)}`);
+      }
+    };
+
+    contentElement.appendChild(gejalaElement);
+  });
+
+  // const diagnoseButton = document.createElement("button");
+  // diagnoseButton.textContent = "Diagnosa";
+  // diagnoseButton.onclick = main;
+  // contentElement.appendChild(diagnoseButton);
+
+  document.querySelector(".button-submit button").addEventListener("click", main);
+
+})
+.catch((error) => console.error("Error:", error));
+
+
+        
